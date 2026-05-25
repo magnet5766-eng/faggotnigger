89f30d3cf0ee90fe518fe04e8be797c1f2fb5efc
magnet5766-eng <magnet5766@gmail.com>
2026-05-25 18:43:06 +1200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,12 +1,21 @@
 (function () {
   var elements = [];
   var lastScrollY = window.scrollY;
+  var ticking = false;
 
   function init() {
     elements = Array.from(document.querySelectorAll('.reveal'));
-    elements.forEach(function (el, i) {
-      var delay = (i % 4) * 0.08;
-      el.style.transitionDelay = delay + 's, ' + delay + 's';
+    var groups = {};
+    elements.forEach(function (el) {
+      var parent = el.parentElement;
+      if (!groups[parent]) groups[parent] = [];
+      groups[parent].push(el);
+    });
+    Object.values(groups).forEach(function (group) {
+      group.forEach(function (el, i) {
+        var d = i * 0.09;
+        el.style.transitionDelay = d + 's, ' + d + 's';
+      });
     });
     check();
   }
@@ -15,27 +24,24 @@
     var scrollY = window.scrollY;
     var scrollingDown = scrollY >= lastScrollY;
     lastScrollY = scrollY;
-
     var viewH = window.innerHeight;
 
     elements.forEach(function (el) {
       var rect = el.getBoundingClientRect();
-      var inView = rect.top < viewH * 0.92 && rect.bottom > viewH * 0.04;
+      var inView = rect.top < viewH * 0.93 && rect.bottom > viewH * 0.05;
 
       if (inView) {
         el.classList.add('visible');
         el.classList.remove('hidden');
-      } else if (rect.bottom <= viewH * 0.04 && !scrollingDown) {
+      } else if (!scrollingDown && rect.bottom < viewH * 0.05) {
         el.classList.remove('visible');
         el.classList.add('hidden');
-      } else if (rect.top >= viewH * 0.92 && scrollingDown) {
+      } else if (scrollingDown && rect.top > viewH * 0.93) {
         el.classList.remove('visible');
         el.classList.remove('hidden');
       }
     });
   }
-
-  var ticking = false;
 
   function onScroll() {
     if (!ticking) {
